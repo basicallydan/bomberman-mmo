880a3bfbed7a9db8eeaa57d0921966828684bfe5
Dan Hough <daniel.hough@gmail.com>
2012-05-12 13:24:56 +0100

    Other players move now

--- client/js/OtherBomberComponent.js
+++ client/js/OtherBomberComponent.js
@@ -8,13 +8,13 @@ Crafty.c('OtherBomber', {
       .animate("walk_down", 0, 3, 2);
     return this;
   },
-  moveBomber : function(from, to) {
+  moveBomber : function(to) {
+    if(this.x < to.x) this.stop().animate("walk_right", 10, -1);
+    else if(this.x > to.x) this.stop().animate("walk_left", 10, -1);
+    else if(this.y < to.y) this.stop().animate("walk_down", 10, -1);
+    else if(this.y > to.y) this.stop().animate("walk_up", 10, -1);
     this.x = to.x;
     this.y = to.y;
-    if(from.x < to.x) this.stop().animate("walk_right", 10, -1);
-    else if(from.x > to.x) this.stop().animate("walk_left", 10, -1);
-    else if(from.y < to.y) this.stop().animate("walk_down", 10, -1);
-    else if(from.y > to.y) this.stop().animate("walk_up", 10, -1);
   },
   getGridPosition : function() {
     return [Math.floor(this.x / 16), Math.floor(this.y /16)];
